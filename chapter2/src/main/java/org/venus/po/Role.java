9920b1b89c687523ce8d3c7a3c0dc5df07265f5a
destiny <chen476328361@163.com>
2018-02-22 00:57:19 +0800

    update

--- chapter2/src/main/java/org/venus/po/Role.java
+++ chapter2/src/main/java/org/venus/po/Role.java
@@ -1,10 +1,12 @@
 package org.venus.po;
 
+import org.apache.ibatis.type.Alias;
 import org.venus.common.ToString;
 
 /**
  * Created by destiny on 2018/2/21.
  */
+@Alias(value = "Role")
 public class Role extends ToString {
     private static final long serialVersionUID = -7264244904820348669L;
 
